baf9c689e6e9e21f7d29bf5affd7be75cb6db67d
slymn_LAB2 <ezfesoftware@gmail.com>
2024-10-01 10:34:52 +0300

    Proje dosyası ekle.

---
+++ bgt_ntp2_hft3_2/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace bgt_ntp2_hft3_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Ekle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_ad = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox_soyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Sehir = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label_SEHIR = new System.Windows.Forms.Label();
+            this.label_SOYAD = new System.Windows.Forms.Label();
+            this.label_AD = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_Ekle
+            // 
+            this.button_Ekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_Ekle.Location = new System.Drawing.Point(6, 108);
+            this.button_Ekle.Name = "button_Ekle";
+            this.button_Ekle.Size = new System.Drawing.Size(219, 90);
+            this.button_Ekle.TabIndex = 0;
+            this.button_Ekle.Text = "Sınıftan Nesne Türet";
+            this.button_Ekle.UseVisualStyleBackColor = true;
+            this.button_Ekle.Click += new System.EventHandler(this.button_Ekle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad";
+            // 
+            // textBox_ad
+            // 
+            this.textBox_ad.Location = new System.Drawing.Point(92, 27);
+            this.textBox_ad.Name = "textBox_ad";
+            this.textBox_ad.Size = new System.Drawing.Size(133, 20);
+            this.textBox_ad.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_Sehir);
+            this.groupBox1.Controls.Add(this.button_Ekle);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox_soyad);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox_ad);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(231, 205);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bilgi Girişi";
+            // 
+            // textBox_soyad
+            // 
+            this.textBox_soyad.Location = new System.Drawing.Point(92, 56);
+            this.textBox_soyad.Name = "textBox_soyad";
+            this.textBox_soyad.Size = new System.Drawing.Size(133, 20);
+            this.textBox_soyad.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Soyad";
+            // 
+            // textBox_Sehir
+            // 
+            this.textBox_Sehir.Location = new System.Drawing.Point(92, 82);
+            this.textBox_Sehir.Name = "textBox_Sehir";
+            this.textBox_Sehir.Size = new System.Drawing.Size(133, 20);
+            this.textBox_Sehir.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(6, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Şehir";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label_SEHIR);
+            this.groupBox2.Controls.Add(this.label_SOYAD);
+            this.groupBox2.Controls.Add(this.label_AD);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(15, 223);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(231, 100);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bilgi Göster";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(6, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Şehir";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(6, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Soyad";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(6, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Ad";
+            // 
+            // label_SEHIR
+            // 
+            this.label_SEHIR.AutoSize = true;
+            this.label_SEHIR.Location = new System.Drawing.Point(58, 79);
+            this.label_SEHIR.Name = "label_SEHIR";
+            this.label_SEHIR.Size = new System.Drawing.Size(31, 13);
+            this.label_SEHIR.TabIndex = 11;
+            this.label_SEHIR.Text = "Şehir";
+            // 
+            // label_SOYAD
+            // 
+            this.label_SOYAD.AutoSize = true;
+            this.label_SOYAD.Location = new System.Drawing.Point(58, 53);
+            this.label_SOYAD.Name = "label_SOYAD";
+            this.label_SOYAD.Size = new System.Drawing.Size(37, 13);
+            this.label_SOYAD.TabIndex = 10;
+            this.label_SOYAD.Text = "Soyad";
+            // 
+            // label_AD
+            // 
+            this.label_AD.AutoSize = true;
+            this.label_AD.Location = new System.Drawing.Point(58, 27);
+            this.label_AD.Name = "label_AD";
+            this.label_AD.Size = new System.Drawing.Size(20, 13);
+            this.label_AD.TabIndex = 9;
+            this.label_AD.Text = "Ad";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 337);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "NTP TEMEL KAVRAMLAR";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Ekle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_ad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_soyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Sehir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label_SEHIR;
+        private System.Windows.Forms.Label label_SOYAD;
+        private System.Windows.Forms.Label label_AD;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ bgt_ntp2_hft3_2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bgt_ntp2_hft3_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Ekle_Click(object sender, EventArgs e)
+        {
+            //OGRENCI ADINDA BİR CLASS OLUSTURUN
+            //FORM1 DE YER ALAN BİLGİLERE GÖRE
+            //CLASS FIELD LARI OLUSUTURUN
+            //CLASS URETİLİRKEN BAŞLANGIÇ BİLGİSİ GİRİLMESİ ZORUNLU OLSUN
+            //NESNE ÜRETİLDDİKTEN SONRA NESNEYE AİT BİLGİLERİ
+            //FORMUN ALTINDA YER ALAN LABEL'LARA YAZDIRIN
+        }
+    }
+}
